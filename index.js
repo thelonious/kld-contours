660e6f3466e846f9c07f381b86e73228a2a9e708
Kevin Lindsey <kevin@kevins-air-2.gateway.2wire.net>
2013-11-12 22:33:30 -0600

    Minor updates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var CubicBezier2D = require('./lib/CubicBezier2D'),
-	QuadraticBezier2D = require('./lib/QuadraticBezier2D');
+	QuadraticBezier2D = require('./lib/QuadraticBezier2D'),
+	Rectangle2D = require('./lib/Rectangle2D');
 
 
 exports.CubicBezier2D = CubicBezier2D;
 exports.QuadraticBezier2D = QuadraticBezier2D;
+exports.Rectangle2D = Rectangle2D;
