fcd0ffa8c04dfa16aa75139f09229172c0761a40
Kevin Lindsey <kevin@kevins-air-2.gateway.2wire.net>
2013-11-12 22:06:36 -0600

    Start basic bezier methods

---
+++ index.js
@@ -0,0 +1,6 @@
+var CubicBezier2D = require('./lib/CubicBezier2D'),
+	QuadraticBezier2D = require('./lib/QuadraticBezier2D');
+
+
+exports.CubicBezier2D = CubicBezier2D;
+exports.QuadraticBezier2D = QuadraticBezier2D;
