e175accb82992fcc2365c78cdc969b1f5c465773
Kevin Lindsey <kevin@kevlindev.com>
2017-07-20 15:49:58 -0700

    Add more contours
    This renames the old Rectangle2D to BoundingBox2D and creates a new Rectangle2D class, along with other shape classes as well.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var CubicBezier2D = require('./lib/CubicBezier2D'),
-	QuadraticBezier2D = require('./lib/QuadraticBezier2D'),
-	Rectangle2D = require('./lib/Rectangle2D');
+exports.BoundingBox2D     = require('./lib/BoundingBox2D');
 
-
-exports.CubicBezier2D = CubicBezier2D;
-exports.QuadraticBezier2D = QuadraticBezier2D;
-exports.Rectangle2D = Rectangle2D;
+// Contours
+exports.Circle2D          = require('./lib/Circle2D');
+exports.CubicBezier2D     = require('./lib/CubicBezier2D');
+exports.Ellipse2D         = require('./lib/Ellipse2D');
+exports.Line2D            = require('./lib/Line2D');
+exports.Polygon2D         = require('./lib/Polygon2D');
+exports.QuadraticBezier2D = require('./lib/QuadraticBezier2D');
+exports.Rectangle2D       = require('./lib/Rectangle2D');
