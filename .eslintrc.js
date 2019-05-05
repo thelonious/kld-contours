8ba6cf8ca5213e224e97d218b9d3fd89a7aee688
Kevin Lindsey <kevin@kevlindev.com>
2019-05-05 13:05:50 -0700

    Update to use latest eslint and docs generation process
    And even more thanks @brettz9!

---
+++ .eslintrc.js
@@ -0,0 +1,80 @@
+"use strict";
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    "extends": ["ash-nazg/sauron-node", "plugin:node/recommended-script"],
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "settings": {
+        "polyfills": [
+            "Error",
+            "Object.defineProperties",
+            "Object.defineProperty"
+        ]
+    },
+    "overrides": [
+        {
+            files: ["**/*.md"],
+            rules: {
+                "eol-last": "off",
+                "no-console": "off",
+                "no-undef": "off",
+                "padded-blocks": "off",
+                "import/unambiguous": "off",
+                "import/no-unresolved": "off",
+                "node/no-missing-import": "off",
+                "no-unused-vars": ["warn"],
+                "no-unused-vars": ["error", {
+                    "varsIgnorePattern": "Point2D"
+                }],
+                "node/no-missing-require": ["error", {
+                    "allowModules": ["kld-intersections"]
+                }]
+            }
+        }
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {"SwitchCase": 1}
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "space-before-function-paren": [
+            "error",
+            "never"
+        ],
+        "brace-style": [
+            "error",
+            "stroustrup"
+        ],
+        "arrow-parens": [
+            "error",
+            "as-needed"
+        ],
+        "no-console": "off",
+        "no-multiple-empty-lines": "off",
+        "max-len": "off",
+        "node/exports-style": "off",
+        "unicorn/no-zero-fractions": "off",
+        "require-unicode-regexp": "off",
+        "yoda": "off",
+        "valid-jsdoc": 0,
+        "import/unambiguous": 0,
+        "global-require": 0,
+        "import/no-commonjs": 0,
+        "consistent-this": "off",
+        "unicorn/prefer-type-error": "off",
+        "node/shebang": "off"
+    }
+};
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-exports.BoundingBox2D     = require('./lib/BoundingBox2D');
+"use strict";
+
+exports.BoundingBox2D = require("./lib/BoundingBox2D");
 
 // Contours
-exports.Circle2D          = require('./lib/Circle2D');
-exports.CubicBezier2D     = require('./lib/CubicBezier2D');
-exports.Ellipse2D         = require('./lib/Ellipse2D');
-exports.Line2D            = require('./lib/Line2D');
-exports.Polygon2D         = require('./lib/Polygon2D');
-exports.QuadraticBezier2D = require('./lib/QuadraticBezier2D');
-exports.Rectangle2D       = require('./lib/Rectangle2D');
+exports.Circle2D = require("./lib/Circle2D");
+exports.CubicBezier2D = require("./lib/CubicBezier2D");
+exports.Ellipse2D = require("./lib/Ellipse2D");
+exports.Line2D = require("./lib/Line2D");
+exports.Polygon2D = require("./lib/Polygon2D");
+exports.QuadraticBezier2D = require("./lib/QuadraticBezier2D");
+exports.Rectangle2D = require("./lib/Rectangle2D");
